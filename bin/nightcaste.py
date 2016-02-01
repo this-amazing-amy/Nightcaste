eb2107a358d78294a049df5d09fb04af3981024b
Christofer Ostwald <christofer.ostwald@gmail.com>
2016-02-01 19:10:35 +0100

    Added simple application start
    - Created the main executable which starts the main method of nightcaste
    - Added empty engine module with a main method

---
+++ bin/nightcaste.py
@@ -0,0 +1,6 @@
+"""Start Nightcaste"""
+
+from nightcaste import engine
+
+if __name__ == "__main__":
+    engine.main()
---
+++ nightcaste/engine.py
@@ -0,0 +1,8 @@
+"""The engine is the main module of the game. It initializes all necessary
+subsystems and runs the super loop"""
+
+__version__ = '0.1.0'
+
+
+def main():
+    print 'Nightcaste v' + __version__
