02a86e6b316da117462c6db96b5d8e2fac566dd3
Nicas Kilian Heydorn <nicas.kilian.heydorn@commerce-plus.com>
2016-02-01 16:12:43 +0100

    Installed libtcod

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Nightcaste',
+            version='0.1.0',
+            description='A 2D Console-based Roguelike game.',
+            author='Nicas Heydorn & Christofer Ostwald',
+            author_email='murmox@gmail.com',
+            url='https://github.com/murmox/nightcaste/',
+            packages=['distutils', 'distutils.command'],
+           )
