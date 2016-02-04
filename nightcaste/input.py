f82d5671a83213e849184e8a27f6a3266cf86045
Nicas Kilian Heydorn <nicas.kilian.heydorn@commerce-plus.com>
2016-02-04 17:19:32 +0100

    Create Player Entity and hooked up input controller with event system

--- nightcaste/input.py
+++ nightcaste/input.py
@@ -1,5 +1,28 @@
 """This module handles user inputs and transforms then into key codes"""
 import tcod as libtcod
+import events
+
+
+def get_event(entity_manager):
+    """ Gets a keycode from input and returns an according event """
+    event = None
+    while event is None:
+        key = wait_for_input(True)
+        event = map_key_to_event(key.vk, entity_manager)
+    return event
+
+
+def map_key_to_event(keycode, entity_manager):
+    """ Determines, which event to throw at the given key """
+    key_actions = {
+        libtcod.KEY_UP: events.MoveAction(entity_manager.player, 0, -1),
+        libtcod.KEY_DOWN: events.MoveAction(entity_manager.player, 0, 1),
+        libtcod.KEY_LEFT: events.MoveAction(entity_manager.player, -1, 0),
+        libtcod.KEY_RIGHT: events.MoveAction(entity_manager.player, 1, 0),
+        # TODO: Make Renderer-Events (for menus, quitting, etc.)
+        libtcod.KEY_ESCAPE: False
+    }
+    return key_actions.get(keycode, None)
 
 
 def wait_for_input(flush):
