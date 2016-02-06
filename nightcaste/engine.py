db13c0f03b23d4b4aaba61823c3c45c2aa88fa16
Christofer Ostwald <christofer.ostwald@gmail.com>
2016-02-06 19:14:43 +0100

    Refactored InputController and fixed double input
    - Input is now handled by class InputController
    - Fixed double input by asking libtcod if the key is pressed
    - Added info and debug logging for engine and InputController

--- nightcaste/engine.py
+++ nightcaste/engine.py
@@ -1,14 +1,19 @@
 """The engine is the main module of the game. It initializes all necessary
 subsystems and runs the super loop"""
-from renderer import SimpleConsoleRenderer
 from events import EventManager
 from entities import EntityManager
 from entities import EntityConfiguration
+from input import InputController
+from nightcaste import __version__
 from processors import MovementProcessor
-import input
+from renderer import SimpleConsoleRenderer
+import logging
+
+logger = logging.getLogger('engine')
 
 
 def main():
+    logger.info('Nightcaste v%s', __version__)
     event_manager = EventManager()
     entity_manager = EntityManager()
     renderer = SimpleConsoleRenderer(entity_manager)
@@ -21,18 +26,18 @@ def main():
     player_config.add_attribute("Position", "y", 0)
     player_config.add_attribute("Renderable", "character", "@")
     entity_manager.player = entity_manager.create_entity_from_configuration(
-                                                                player_config)
+        player_config)
+    input_controller = InputController(entity_manager)
     # TODISCUSS: Do we need to save the Listeners?
-    event_manager.register_listener("MoveAction",
-                                    MovementProcessor(
-                                        event_manager, entity_manager
-                                    )
-                                    )
+    event_manager.register_listener(
+        "MoveAction", MovementProcessor(
+            event_manager, entity_manager))
     renderer.render()
     while renderer.is_active():
-        round += 1
+        round += 1l
+        logger.info('Round: %s', round)
         # TODISCUSS: Pass the and entity manager to input?
-        input_event = input.get_event(entity_manager)
+        input_event = input_controller.get_event()
         if input_event is False:
             break
         event_manager.enqueue_event(input_event)
