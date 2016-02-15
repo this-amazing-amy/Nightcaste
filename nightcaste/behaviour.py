a7107813461a626e1ce4cea6d261186727b947cd
14122 <nicas.heydorn@nordakademie.de>
2016-02-15 13:24:19 +0100

    Fixed some typos

--- nightcaste/behaviour.py
+++ nightcaste/behaviour.py
@@ -8,7 +8,7 @@ class BehaviourManager:
     def __init__(self, event_manager, entitiy_manager, config=None):
         self.event_manager = event_manager
         self.entity_manager = entitiy_manager
-        self.behaviors = {}
+        self.behaviours = {}
         if config is not None:
             self.configure(config)
 
@@ -21,11 +21,11 @@ class BehaviourManager:
         """
         if 'component_behaviours' in config:
             for behaviour in config['component_behaviours']:
-                self.add_comp_behavoir_from_name(
+                self.add_comp_behaviour_from_name(
                     behaviour['component_type'],
                     behaviour['name'])
 
-    def add_comp_behavoir_from_name(self, component_type, behaviour_name):
+    def add_comp_behaviour_from_name(self, component_type, behaviour_name):
         """Associates the behaviour specified be the name with the specified
         compoenent_type.
 
@@ -38,16 +38,16 @@ class BehaviourManager:
         module = importlib.import_module('nightcaste.behaviour')
         behaviour_class = getattr(module, behaviour_name)
         behaviour = behaviour_class(self.event_manager, self.entity_manager)
-        self.add_component_bevaiour(component_type, behaviour)
+        self.add_component_behaviour(component_type, behaviour)
 
-    def add_component_bevaiour(self, component_type, behaviour):
+    def add_component_behaviour(self, component_type, behaviour):
         """Register the given behaviour with the specified component type."""
-        self.behaviors.update({component_type: behaviour})
+        self.behaviours.update({component_type: behaviour})
 
     def update(self, round, delta_time):
         """Updates all behaviours for all entitites with a associated
         components."""
-        for component_type, behaviour in self.behaviors.iteritems():
+        for component_type, behaviour in self.behaviours.iteritems():
             components = self.entity_manager.get_all_of_type(component_type)
             for entity, component in components.iteritems():
                 behaviour.entity = entity
