a8dcd7a63852e991ef22a293aed1c7b876cf2239
Christofer Ostwald <christofer.ostwald@gmail.com>
2016-03-19 17:43:14 +0100

    Enhanced QuadTree
    - Added a layer on top af the quadtree nodes which holds a reference to
      the leafe of each added item, this enables the tree to quickly find
      the current node of an entity
    - Added parent in order to move the tree upwards
    - Added Delete and Move support for entiies in the tree
    - Added support for collapsing empty nodes on move and delete
    - Added MapChanged Event
    - The MovementSystem now fills the collision tree on map changed event
    - The MovementSystem now updates the position in the quadtree when an
      entity has moved

--- nightcaste/collision.py
+++ nightcaste/collision.py
@@ -3,28 +3,81 @@ from pygame import Rect
 
 class QTreeCollisionManager:
 
-    def update(self, bounds, collidables):
+    def fill(self, bounds, collidables):
         """Add all collidable objects to a quad tree. O(n * log n)"""
-        self.qtree = QuadTree(0, bounds)
+        self.qtree = QuadTree(bounds)
         for entity, collidable in collidables.iteritems():
-            self.qtree.insert(collidable, entity)
+            self.qtree.insert(entity, collidable)
+
+    def move(self, entity):
+        """Notifies the collision manager that an entits rect was moved. The
+        entity will be relocated in the quad tree.
+
+        NOTE: The methods assumes the entites rect was moved in place!"""
+        self.qtree.move(entity)
 
     def collide_rect(self, entity, rect):
-        collisions = []
-        possible_collisions = {}
-        self.qtree.retrieve(possible_collisions, rect)
-        for other_entity, other_rect in possible_collisions.iteritems():
-            if entity == other_entity:
-                continue
-            if rect.colliderect(other_rect):
-                collisions.append(other_entity)
+        collisions = self.qtree.retrieve(rect)
+        try:
+            collisions.remove(entity)
+        except ValueError:
+            pass
         return collisions
 
 
+class QuadTreeObject:
+
+    def __init__(self, rect):
+        self.rect = rect
+        self.owner = None
+
+
 class QuadTree:
+
+    def __init__(self, bounds, max_entites=5, max_level=5):
+        self.entites = {}
+        self.q_tree_root = QuadTreeNode(None, 0, bounds, max_entites, max_level)
+
+    def bounds(self):
+        return self.q_tree_root.bounds
+
+    def get_all(self):
+        return self.entites.viewkeys()
+
+    def contains(self, entity):
+        return entity in self.entites
+
+    def count(self):
+        return len(self.entites)
+
+    def retrieve(self, rect):
+        colliding_entities = []
+        self.q_tree_root.retrieve(rect, colliding_entities)
+        return colliding_entities
+
+    def insert(self, entity, rect):
+        q_tree_object = QuadTreeObject(rect)
+        self.entites[entity] = q_tree_object
+        self.q_tree_root.insert(entity, q_tree_object)
+
+    def remove(self, entity):
+        if self.contains(entity):
+            self.q_tree_root.delete(entity, self.entites.pop(entity))
+            return True
+        return False
+
+    def move(self, entity):
+        if self.contains(entity):
+            self.q_tree_root.move(entity, self.entites[entity])
+            return True
+        return False
+
+
+class QuadTreeNode:
     """Implements a static QuadTree for collision detection."""
 
-    def __init__(self, level, bounds, max_entites=5, max_level=5):
+    def __init__(self, parent, level, bounds, max_entites=5, max_level=5):
+        self.parent = None
         self.level = level
         self.bounds = bounds
         self.max_entities = max_entites
@@ -40,26 +93,31 @@ class QuadTree:
         y = self.bounds.y
 
         self.nodes = [
-            QuadTree(
+            QuadTreeNode(
+                self,
                 self.level + 1,
                 Rect(x + sub_w, y, sub_w, sub_h),
                 self.max_entities,
                 self.max_level),
-            QuadTree(
+            QuadTreeNode(
+                self,
                 self.level + 1,
                 Rect(x, y, sub_w, sub_h),
                 self.max_entities,
                 self.max_level),
-            QuadTree(
+            QuadTreeNode(
+                self,
                 self.level + 1,
                 Rect(x, y + sub_h, sub_w, sub_h),
                 self.max_entities,
                 self.max_level),
-            QuadTree(
+            QuadTreeNode(
+                self,
                 self.level + 1,
                 Rect(x + sub_w, y + sub_h, sub_w, sub_h),
                 self.max_entities,
                 self.max_level)]
+        self._distribute()
 
     def _get_index(self, rect):
         """Determine which node the object belongs to. -1 means object cannot
@@ -88,48 +146,90 @@ class QuadTree:
 
         return index
 
-    def insert(self, rect, entity):
+    def insert(self, entity, item):
         """Insert the object into the quadtree. If the node exceeds the
         capacity, it will split and add all objects to their corresponding
         nodes."""
         if self.nodes is not None:
-            index = self._get_index(rect)
+            index = self._get_index(item.rect)
             if index != -1:
-                self.nodes[index].insert(rect, entity)
+                self.nodes[index].insert(entity, item)
                 return
 
-        self.entites[entity] = rect
+        self.entites[entity] = item
+        item.owner = self
         if len(self.entites) > self.max_entities and self.level < self.max_level:
             if self.nodes is None:
                 self.split()
-            self.redistribute()
 
-    def redistribute(self, deep=False):
+    def _distribute(self):
         """Checks of the entites in this node fits into a subnode and inserts
-        it.
-
-        Args:
-            deep (bool): If True calls redistribute on the whole subtree.
-        """
-        if deep and self.nodes is not None:
-            for node in self.nodes:
-                node.redistribute(deep)
-
-        # TODO find better way than making a copy
-        for entity, rect in self.entites.items():
-            index = self._get_index(rect)
+        it."""
+        # Do not copy any items since the refenrences to the orignal rects must
+        # stay valid for move/relocate
+        for entity in self.entites.keys():
+            index = self._get_index(self.entites[entity].rect)
             if index != -1:
-                del self.entites[entity]
-                self.nodes[index].insert(rect, entity)
+                self.nodes[index].insert(entity, self.entites.pop(entity))
 
-    def retrieve(self, entity_dict, rect):
-        """Fills the given dict with all objects that could collide with the
-        given object."""
+    def retrieve(self, rect, result):
+        """Fills the given list with all objects that collide with the given
+        rect."""
         if self.nodes is not None:
             index = self._get_index(rect)
             if index != -1:
-                self.nodes[index].retrieve(entity_dict, rect)
-        entity_dict.update(self.entites)
+                self.nodes[index].retrieve(rect, result)
+        for entity, item in self.entites.iteritems():
+            if rect.colliderect(item.rect):
+                result.append(entity)
+
+    def delete(self, entity, item, clean=True):
+        """Removes an item from its node."""
+        if item.owner is not None:
+            if item.owner == self:
+                del self.entites[entity]
+                if clean:
+                    self._clean_upwards()
+            else:
+                item.owner.delete(entity, item)
+
+    def move(self, entity, item):
+        """Calls _relacote on the items current node."""
+        if item.owner is not None:
+            item.owner._relocate(entity, item)
+
+    def _relocate(self, entity, item):
+        """Moves an item with the tree. If the item doesn't fit into this leaf
+        anymore, the call will be delegated to the parent."""
+        # do we fit into this node
+        if (self.bounds.contains(item.rect)):
+            # do we fit into our childs
+            if self.nodes is not None:
+                index = self._get_index(item.rect)
+                if index != -1:
+                    dest = self.nodes[index]
+                    if item.owner != dest:
+                        # remove from old node and add to our child
+                        former_owner = item.owner
+                        # delay cleanup since it could delete our destination
+                        self.delete(entity, item, False)
+                        dest.insert(entity, item)
+                        # delyed cleanup
+                        former_owner._clean_upwards()
+        elif self.parent is not None:
+            self.parent._relocate(item)
+
+    def _clean_upwards(self):
+        """Checks if the childs are empty and deletes them. If this Node is
+        empty, the call will be delegated to the parent."""
+        if self.nodes is not None:
+            all_empty = True
+            for node in self.nodes:
+                all_empty &= node.is_empty()
+            if all_empty:
+                self.nodes = None
+        if self.parent is not None and self.is_empty:
+            self.parent._clean_upward()
 
     def height(self):
         """Calculate the height of the tree. The height is the maximum height of
@@ -149,6 +249,9 @@ class QuadTree:
                 count += node.count()
         return count
 
+    def is_empty(self):
+        return self.count() == 0
+
     def __str__(self):
-        return 'QuadTree(%s) height: %d count: %d' % (
-            self.bounds, self.height(), self.count())
+        return 'QuadTreeNode(level: %d, bounds: %s, entities: %d)'  % (
+            self.level, self.bounds, len(self.entites))
