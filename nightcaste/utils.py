8a16e2d9644f763708ddee21a04cb0404d6995f7
Christofer Ostwald <christofer.ostwald@gmail.com>
2016-03-26 16:25:48 +0100

    Added ability to directly load classes from their specification

--- nightcaste/utils.py
+++ nightcaste/utils.py
@@ -7,6 +7,11 @@ def class_for_name(module_name, class_name):
     return getattr(module, class_name)
 
 
+def get_class(class_specification):
+    spec = class_specification.rsplit('.', 1)
+    return class_for_name(spec[0], spec[1])
+
+
 def load_config(config_path):
     with open(config_path) as config_file:
         return json.load(config_file)
